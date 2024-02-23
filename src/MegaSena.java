26aabe40baaaaf1f26dcc046c8232359b356a7f5
André Souza <andresouza_ti@yahoo.com>
2024-02-23 17:44:51 -0300

    atualizado

--- src/MegaSena.java
+++ src/MegaSena.java
@@ -1,18 +1,3 @@
-//import java.util.Random;
-//
-//public class MegaSena {
-//    public static void main(String[] args) {
-//        Random generate = new Random();
-//
-//        int i = 0;
-//        while(i < 6) {
-//            int number = generate.nextInt(60);
-//            System.out.println(number);
-//            i++;
-//        }
-//    }
-//}
-
 import java.util.Random;
 import java.util.HashSet;
 import java.util.Set;
